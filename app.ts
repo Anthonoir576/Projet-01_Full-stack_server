b0273ecbf7b18e8971273989e8bfcbfc9ac97421
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-22 15:46:37 +0200

    :FIX: => gros probleme

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 
 // ------ IMPORT ------
-import express from 'express';
+import express            from 'express';
 // --------------------
 
 
@@ -12,13 +12,6 @@ const router  = require('./src/routes/router');
 
 
 
-// ------ CONFIGURATION ------
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// ---------------------------
-
-
-
 // ------ ROUTES ------
-app.use(router);
+app.use('/', router);
 // --------------------
