b286782d1b3e74e7fb802dc848ecd98e85236eb8
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-22 08:52:45 +0200

    :FEAT: => serveur ok mise en place dune architecture propre et lisible

--- server.ts
+++ server.ts
@@ -1,8 +1,23 @@
+
+// ------ IMPORT ------
 import express  from 'express';
 import http     from 'http';
 
+require('dotenv').config({ path: './src/config/.env' });
+// --------------------
 
+
+
+// ------ VARIABLE ------
 const socketio  = require('socket.io');
 const app       = express();
 const server    = http.createServer(app);
 const io        = socketio(server);
+const PORT      = process.env.PORT || 5000;
+// ---------------------
+
+
+
+// ------ LANCEMENT SERVEUR ------
+server.listen(PORT, () => console.log(`le serveur démarre sur le port ${PORT}`));
+// -------------------------------  
