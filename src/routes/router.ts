6148ca8d3f7c99d4dfc4f9f1016f691f0607282f
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-22 09:01:44 +0200

    :FEAT: => mise en place route en cours

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -8,3 +8,13 @@ import express  from "express";
 // ------ VARIABLE ------
 const router = express.Router();
 // ----------------------
+
+
+
+// ------ ROUTES ------
+
+router.get('/', (req? :any, res? :any) => {
+    console.log('salut ca marche');
+});
+
+// --------------------
