1507c561e0737787144ae2a0a2ca3717db976b13
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-23 04:05:32 +0200

    :BUG: => Tentative de modification en cours car bug sur l'api socket.io et ma logique

--- app.ts
+++ app.ts
@@ -1,17 +1,23 @@
 
 // ------ IMPORT ------
 import express            from 'express';
+import { corsControls }   from "./src/config/cors";
 // --------------------
 
 
 
 // ------ VARIABLE ------
 const app     = express();
+const cors    = require('cors');
 const router  = require('./src/routes/router');
 // ---------------------
 
 
 
 // ------ ROUTES ------
+app.use(cors(corsControls));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/', router);
 // --------------------
