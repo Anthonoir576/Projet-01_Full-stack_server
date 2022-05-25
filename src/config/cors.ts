bc15fc539bd906434c321592bc0e64c6c56b238a
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-25 03:55:08 +0200

    :BUG: => Bug toujous present - la doc dit qu'il faut pas mettre le * mais pour le moment toujours erreur cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,10 +3,6 @@ require('dotenv').config({ path: './.env' });
 
 
 export const corsOptions: any = {
-    origin             : '*',
-    credentials        : true,
-    'allowedHeaders'   : ['sessionId', 'Content-Type', 'Origin', 'X-Requested-With', 'X-Auth-Token', 'Content', 'Accept', 'Authorization' ],
-    'exposedHeaders'   : ['sessionId'],
-    'methods'          : 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    'preflightContinue': false
+    origin             : 'http://localhost:3000/Projet-01_Full-stack_client/*',
+    credentials        : true
 };
