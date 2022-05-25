354bf44b5589b94378f597199d75cddb89ed5cbf
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-25 04:29:58 +0200

    :BUG: => Bug toujous present - En cours de test

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,6 +1,8 @@
 
 require('dotenv').config({ path: './.env' });
 
+// λ curl "http://localhost:5000/socket.io/?EIO=4&transport=polling"
+// 0{"sid":"bF_sWgeqhDNxHt_LAAAY","upgrades":["websocket"],"pingInterval":25000,"pingTimeout":20000,"maxPayload":1000000}
 
 export const corsOptions: any = {
     'Access-Control-Allow-Origin': ['http://localhost:3000/Projet-01_Full-stack_client', 'http://localhost:3000'],
