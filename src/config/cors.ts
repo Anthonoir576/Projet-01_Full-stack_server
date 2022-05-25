61ef8fac5e57d6077d3cfb4fc0977dba693516f5
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-25 04:17:45 +0200

    :BUG: => Bug toujous present - En cours de test

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,11 @@ require('dotenv').config({ path: './.env' });
 
 
 export const corsOptions: any = {
-    origin             : 'http://localhost:3000/Projet-01_Full-stack_client/*',
-    credentials        : true
+    origin             : 'http://localhost:3000/Projet-01_Full-stack_client',
+    credentials        : true,
+    // allowedHeaders: ["monHeader"],
+    // 'allowedHeaders'   : ['sessionId', 'Content-Type', 'Origin', 'X-Requested-With', 'X-Auth-Token', 'Content', 'Accept', 'Authorization' ],
+    // 'exposedHeaders'   : ['sessionId'],
+    // 'methods'          : 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    // 'preflightContinue': false
 };
