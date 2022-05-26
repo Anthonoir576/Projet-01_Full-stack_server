64bd569925f98924642a632869e092d55fab4332
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-26 12:52:35 +0200

    :FEAT: => middleware addUser fini + qua

---
+++ src/controllers/userCTRL.ts
@@ -0,0 +1,44 @@
+const users :any = [];
+
+
+
+const addUser = ({id, name, room} :any) => {
+
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+
+    const existingUser = users.find((user? :any) => {
+        user.room === room && user.name === name
+    });
+
+
+    if (existingUser) {
+        return { error: 'Utilisateur déjà existant !' };
+    };
+
+    const user = { id: id, name: name, room: room };
+
+    users.push(user);
+
+    return { user };
+};
+
+const removeUser = (id :number) => {
+    
+};
+
+
+
+
+
+
+const getUser = () => {
+
+};
+
+
+const getUsersInRoom = () => {
+
+};
+
